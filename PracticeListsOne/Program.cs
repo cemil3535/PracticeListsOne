1249191cd6c28cc65445e32a8b09d06ac3546534
cemil3535 <cemilozcan35@gmail.com>
2024-08-16 00:56:23 +0300

    Add project files.

---
+++ PracticeListsOne/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("** Davetliler **");
+
+// List created and value appointment
+List<string> fullNames= new List<string>() { "1- Bulent Ersoy", "2- Ajda Pekkan", "3- Ebru Gundes", "4- Hadise", "5- Hande Yener", "6- Tarkan", "7- Funda Arar", "8- Demet Akalin"};
+
+// list elemen console write
+foreach (string fullNameItem in fullNames)
+{
+    Console.WriteLine(fullNameItem);
+}
